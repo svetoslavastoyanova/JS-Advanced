b77d4900facc3576c6f2043263d7c9915995a6d2
svetoslavastoyanova <svetoslava_stoyanova92@abv.bg>
2021-10-12 15:52:48 +0300

    added new files

---
+++ Arrays_And_Nested_Arrays/08.sortAnArray.js
@@ -0,0 +1,19 @@
+function solve(arr) {
+    const result = arr.sort((a,b) => {
+ 
+        if (a.length > b.length) {
+            return 1;
+        } else if (a.length== b.length) {
+            return a.localeCompare(b);
+        } else {
+            return -1;
+        }
+    })
+    console.log(result.join("\n"));
+}
+
+solve(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George'])
