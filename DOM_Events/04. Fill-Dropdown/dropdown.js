01f8570b812cc12e15dcdfd220d51c5f5b1d7308
svetoslavastoyanova <svetoslava_stoyanova92@abv.bg>
2021-11-29 16:15:27 +0200

    added new files

---
+++ DOM_Events/04. Fill-Dropdown/dropdown.js
@@ -0,0 +1,12 @@
+function addItem() {
+    let text = document.getElementById('newItemText');
+    let value = document.getElementById('newItemValue');
+    let select = document.getElementById('menu');
+    let option = document.createElement('option');
+
+    option.value = value.value;
+    option.textContent = text.value;
+    select.appendChild(option);
+    text.value = '';
+    value.value = '';
+}
