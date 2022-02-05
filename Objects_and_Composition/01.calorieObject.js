6d79e88877d5d353b59d120778c723e85dd5fa09
svetoslavastoyanova <svetoslava_stoyanova92@abv.bg>
2022-02-05 13:14:08 +0200

    new files added

---
+++ Objects_and_Composition/01.calorieObject.js
@@ -0,0 +1,11 @@
+function calorie(arr) {
+    let result = {};
+
+    for (let i = 0; i < arr.length; i += 2) {
+        result[arr[i]] = Number(arr[i + 1]);
+    }
+
+    return result;
+}
+
+calorie(['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])
