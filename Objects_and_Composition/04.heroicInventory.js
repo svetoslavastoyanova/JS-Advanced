aafa5fe2e027e02912b34e35878bcf7fdd66b303
svetoslavastoyanova <svetoslava_stoyanova92@abv.bg>
2022-02-15 14:26:17 +0200

    new files added

---
+++ Objects_and_Composition/04.heroicInventory.js
@@ -0,0 +1,17 @@
+function solve(arr) {
+    const result = [];
+
+    for (let el of arr) {
+        let [name, level, items] = el.split(' / ');
+        level = Number(level);
+        items = items ? items.split(', ') : [];
+
+        result.push({ name, level, items });
+    }
+
+    console.log(JSON.stringify(result));
+}
+
+console.log(solve(['Isacc / 25 / Apple, GravityGun',
+'Derek / 12 / BarrelVest, DestructionSword',
+'Hes / 1 / Desolator, Sentinel, Antara']))
