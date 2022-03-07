41fa91ca3c12c227889ecbbc57d4f84ad75c1a1a
svetoslavastoyanova <svetoslava_stoyanova92@abv.bg>
2022-03-07 13:59:08 +0200

    added new files

---
+++ Prototypes_and_Inheritance/03.extensibleObject.js
@@ -0,0 +1,24 @@
+function extensibleObject() {
+    const proto = {};
+    const inst = Object.create(proto);
+  
+    inst.extend = function (templates) {
+      Object.entries(templates).forEach(([key, value]) => {
+        if (typeof value === "function") {
+          proto[key] = value;
+        } else {
+          inst[key] = value;
+        }
+      });
+    };
+  
+    return inst;
+}
+
+const myObj = extensibleObject(); 
+const template = { 
+      extensionMethod: function () {}, 
+      extensionProperty: 'someString' 
+    } 
+myObj.extend(template);
+console.log(myObj);
