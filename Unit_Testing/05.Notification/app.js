2148b426cf4538621f00b9e26e0d75c62a916ef7
svetoslavastoyanova <svetoslava_stoyanova92@abv.bg>
2022-04-30 12:51:23 +0300

    new files added

---
+++ Unit_Testing/05.Notification/app.js
@@ -0,0 +1,8 @@
+function notify(message) {
+  let div = document.getElementById('notification');
+  div.textContent = message;
+  div.style.display = 'block';
+  div.addEventListener('click', (e) => {
+     e.target.style.display = 'none';
+  })
+}
